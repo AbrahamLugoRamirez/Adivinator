c66aa634304741258c746f42dedaac465710487c
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2020-04-27 14:35:27 -0500

    function

--- js/script.js
+++ js/script.js
@@ -7,8 +7,15 @@ window.location = "../game.html"
 }
 var cont = 0;
 var val = false;
+var vall = false;
 
 function he(opc){
+    if(vall == true){
+        otras(opc);
+    }
+    if(val == true){
+        otra(opc);
+    }
     if(cont < 4){
         var bt = opc;
         console.log(opc)
@@ -16,18 +23,14 @@ function he(opc){
         cont += 1;
         mitad();
     }
-    if(val == true){
-        otra(opc);
-    }
     
+    console.log(cont);    
 }
 console.log(cont)
 
 function mitad(){
     if(cont == 4){
-        var a = localStorage.getItem("num");
-        
-        
+        var a = localStorage.getItem("num");       
         if(document.getElementById("h"+a).disabled == true){
         Swal.fire({
             position: 'center',
@@ -36,9 +39,6 @@ function mitad(){
             showConfirmButton: false,
             timer: 1500
           })
-
-
-
         }else{
             Swal.fire({
                 position: 'center',
@@ -49,21 +49,76 @@ function mitad(){
               })
               val = true;
 
-        }
-       
-
-
-       
+        }       
     }
 }
 function otra(opc){
-    if(cont <= 6){
+    if(cont < 6){
         var bt = opc;
         console.log(opc)
         document.getElementById(opc).disabled=true;
         cont += 1;
-        
+        mitadd();        
     }
-
-
 }
+
+function mitadd(){
+    if(cont == 6){
+        var a = localStorage.getItem("num");       
+        if(document.getElementById("h"+a).disabled == true){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Se mantendran los que selecionaste',
+            showConfirmButton: false,
+            timer: 1500
+          })
+
+        }else{
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Se eliminaran los selecionados, escoge dos mas',
+                showConfirmButton: false,
+                timer: 1500
+              }) 
+              vall = true;            
+
+        }
+       
+    }
+}
+function otras(opc){
+    if(cont < 7){
+        var bt = opc;
+        console.log(opc)
+        document.getElementById(opc).disabled=true;
+        cont += 1;
+        final();      
+    }
+}
+
+function final(){
+    var a = localStorage.getItem("num");       
+        if(document.getElementById("h"+a).disabled == true){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'El numero es el que selecionaste: ' + a,
+            showConfirmButton: false,
+            timer: 1500
+          })
+
+        }else{
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'El numero es el ' + a,
+                showConfirmButton: false,
+                timer: 1500
+              }) 
+              vall = true;
+
+
+            }
+        }
